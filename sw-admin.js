d81903855f0854d60b5aa36a4eafe09e3e37dc70
solucionesmedicasintegral-rgb <solucionesmedicasintegral@gmail.com>
2026-09-04 17:36:50 -0600

    Add files via upload

---
+++ sw-admin.js
@@ -0,0 +1,23 @@
+const C='hector-admin-pwa-v30';
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll([
+    './admin.html',
+    './config.js',
+    './services.js',
+    './manifest-admin.json',
+    './assets/logo-admin.png',
+    './assets/icon-admin-192.png',
+    './assets/icon-admin-512.png',
+    './assets/apple-touch-icon-admin.png'
+  ])));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
